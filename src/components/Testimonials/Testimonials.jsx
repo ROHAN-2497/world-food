a2e6f1d81e4f9df3e0b1d26cc0db07e089568f03
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-23 18:22:32 +0600

    update

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,4 +1,7 @@
+import { Rating } from "@smastrom/react-rating";
 import { useEffect, useState } from "react";
+import "@smastrom/react-rating/style.css";
+import { CiHeart } from "react-icons/ci";
 
 const Testimonials = () => {
   const [reviews, setReviews] = useState([]);
@@ -14,13 +17,38 @@ const Testimonials = () => {
         <h2 className="text-3xl">FRESH FROM OUR COMMUNITY</h2>
         <h4 className="text-1xl font-medium">View All</h4>
       </div>
-      <div>
+      <div className="flex gap-4">
         {reviews.map((review) => (
-          <div key={review._id}>
-            <div>
+          <div className="border p-4" key={review._id}>
+            <div className="flex justify-between">
               {" "}
-              <img src={review.profile_image} alt="" />
+              <img
+                className="w-10 h-10 rounded-full "
+                src={review.profile_image}
+                alt=""
+              />
               <p>{review.name}</p>
+            </div>
+            <div>
+              <img src={"/public/O3flKPWrS0eQVldjLMwB_IMG_0423.webp"} alt="" />
+              <Rating
+                className=""
+                style={{ maxWidth: 80 }}
+                value={5}
+                readOnly
+              />
+              <p>
+                Delicious. I like patties cooked in pan with a little olive oil.
+                I used ground chuck roast.
+              </p>
+            </div>
+            <div>
+              <p>58 MINUTES AGO</p>
+              <div className="flex justify-between">
+                {" "}
+                <p className="font-semibold">REPLAY</p>
+                <CiHeart />
+              </div>
             </div>
           </div>
         ))}
