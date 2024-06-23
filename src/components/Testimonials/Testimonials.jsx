fc730d50e2c78a1c107ddce221b3c3b04d62d2a6
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-23 07:58:45 +0600

    update json file for review

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from "react";
+
 const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
   return (
     <div className="w-[85%] mx-auto">
       <div className="flex  items-center justify-between ">
@@ -6,7 +14,17 @@ const Testimonials = () => {
         <h2 className="text-3xl">FRESH FROM OUR COMMUNITY</h2>
         <h4 className="text-1xl font-medium">View All</h4>
       </div>
-      <div></div>
+      <div>
+        {reviews.map((review) => (
+          <div key={review._id}>
+            <div>
+              {" "}
+              <img src={review.profile_image} alt="" />
+              <p>{review.name}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
